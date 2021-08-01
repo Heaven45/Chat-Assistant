902645d61147fc61b5813cd1bae39944a3beff22
s.fedulov <s.fedulov@corp.vk.com>
2021-08-01 20:59:58 +0300

    Add pods with adapter

--- app/src/main/java/com/example/chatassistant/MainActivity.kt
+++ app/src/main/java/com/example/chatassistant/MainActivity.kt
@@ -5,13 +5,40 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Adapter
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.TextView
 import com.example.chatassistant.databinding.ActivityMainBinding
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
     val TAG: String = "MainActivity"
+
+    lateinit var requestInput: TextInputEditText
+
+    lateinit var podsAdapter: SimpleAdapter
+
+    val pods = mutableListOf<HashMap<String, String>>(
+        HashMap<String, String>().apply {
+            put("Title", "Title 1")
+            put("Content", "Content 1")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 2")
+            put("Content", "Content 2")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 3")
+            put("Content", "Content 3")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 4")
+            put("Content", "Content 4")
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +50,18 @@ class MainActivity : AppCompatActivity() {
     fun initViews() {
         val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        requestInput = findViewById(R.id.text_input_edit)
+
+        val podsList: ListView = findViewById(R.id.pods_list)
+        podsAdapter = SimpleAdapter(
+            applicationContext,
+            pods,
+            R.layout.item_pod,
+            arrayOf("Title", "Content"),
+            intArrayOf(R.id.title, R.id.content)
+        )
+        podsList.adapter = podsAdapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
