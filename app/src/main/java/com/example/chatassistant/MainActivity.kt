bf2af178e2aef204b0edec73d23fc229d350d0a2
s.fedulov <s.fedulov@corp.vk.com>
2021-08-01 15:00:23 +0300

    Add toolbar and menu

--- app/src/main/java/com/example/chatassistant/MainActivity.kt
+++ app/src/main/java/com/example/chatassistant/MainActivity.kt
@@ -3,33 +3,44 @@ package com.example.chatassistant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import com.example.chatassistant.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
-    val name: String = "Ivan"
-    val surname: String = "Ivanov"
-    var age: Int = 37
-    var height: Double = 172.2
-
-    var finalText: String = "name: $name surname: $surname age: $age height: $height"
-
-    private lateinit var binding: ActivityMainBinding
     val TAG: String = "MainActivity"
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "start of onCreate function")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        binding.output.text = finalText
-        Log.d(TAG, "end of onCreate function")
+        initViews()
     }
+
+    fun initViews() {
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_stop -> {
+                Log.d(TAG, "ACTION STOP")
+                return true
+            }
+            R.id.action_clear -> {
+                Log.d(TAG, "ACTION CLEAR")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
